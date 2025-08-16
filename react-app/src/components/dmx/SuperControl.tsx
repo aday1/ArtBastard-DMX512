f1c8f0d01d3baca3417b14003c93bb2ac07b6a45
Aday <aday@aday.net.au>
2025-08-17 00:07:00 +1000

    STABLE ARTBASTARD SUCCESS!

--- react-app/src/components/dmx/SuperControl.tsx
+++ react-app/src/components/dmx/SuperControl.tsx
@@ -6,7 +6,7 @@ import { Responsive, WidthProvider } from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 
-const ResponsiveGridLayout = WidthProvider(Responsive);
+const ResponsiveGridLayout = WidthProvider(Responsive) as any;
 
 interface SuperControlProps {
   isDockable?: boolean;
